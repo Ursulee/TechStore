f13e0347217f9543a374223cfd4ede44ba3a416d
_therax <nabilmoutouama@gmail.com>
2025-12-10 17:25:43 +0100

    Mise à jour du fichier navigation+gestion des conflits et dupplication de classes

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -13,56 +13,26 @@ import 'package:code_initial/presentation/pages/auth/code_verification/code_veri
 import 'package:code_initial/presentation/pages/auth/mot_de_passe_oublie/mot_de_passe_oublie_page.dart';
 
 // === Import Controllers ===
-import 'package:code_initial/presentation/pages/auth/code_verification/code_verification_controller.dart';
 import 'package:code_initial/presentation/pages/auth/mot_de_passe_oublie/mot_de_passe_oublie_controller.dart';
 import 'package:get/get.dart';
 
-class Nav {
-  static List<GetPage> routes = [
-    GetPage(
-      name: Routes.CATALOGUE,
-      page: () => const CataloguePage(),
-      binding: BindingsBuilder(() {
-        Get.lazyPut<CatalogueController>(() => CatalogueController());
-      }),
-    ),
-    GetPage(
-      name: Routes.PROFIL,
-      page: () => const ProfilPage(),
-      binding: BindingsBuilder(() {
-        Get.lazyPut<ProfilController>(() => ProfilController());
-      }),
-    ),
-    GetPage(
-      name: Routes.REGISTER,
-      page: () => RegisterPage(),
-    ),
-  ];
-}
-
-
 class Routes {
-  static Future<String> get initialRoute async {
-    return CATALOGUE;
   // ROUTES
   static const SPLASH = "/";
   static const CONNEXION = "/connexion";
   static const INSCRIPTION = "/inscription";
   static const REGISTER = '/register';
   static const OTPCODE = "/auth/code_verification";
-  static const MDPFORGET ="/auth/mot_de_passe_oublie";
+  static const MDPFORGET = "/auth/mot_de_passe_oublie";
+  static const CATALOGUE = '/catalogue';
+  static const PROFIL = '/profil';
 
   // ROUTE INITIALE
   static Future<String> get initialRoute async {
-    // Tu peux mettre la logique que tu veux ici
     return SPLASH;
   }
 }
 
-  static const String MAIN = "/";
-  static const String REGISTER = '/register';
-  static const String CATALOGUE = '/catalogue';
-  static const String PROFIL = '/profil';
 class Nav {
   static List<GetPage> routes = [
     GetPage(
@@ -75,7 +45,7 @@ class Nav {
     ),
     GetPage(
       name: Routes.INSCRIPTION,
-      page: () => PageInscription(),
+      page: () => const PageInscription(),
     ),
     GetPage(
       name: Routes.MDPFORGET,
@@ -90,5 +60,20 @@ class Nav {
       name: Routes.REGISTER,
       page: () => RegisterPage(),
     ),
+    GetPage(
+      name: Routes.CATALOGUE,
+      page: () => const CataloguePage(),
+      binding: BindingsBuilder(() {
+        Get.lazyPut<CatalogueController>(() => CatalogueController());
+      }),
+    ),
+    GetPage(
+      name: Routes.PROFIL,
+      page: () => const ProfilPage(),
+      binding: BindingsBuilder(() {
+        Get.lazyPut<ProfilController>(() => ProfilController());
+      }),
+    ),
   ];
 }
+
